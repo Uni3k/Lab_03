2d401f424470df5205ed087b29b75a6b8eebe63c
Uni3k <91822418+Uni3k@users.noreply.github.com>
2021-10-17 09:23:47 +0200

    Add files via upload

---
+++ Lab3 zad 1.java
@@ -0,0 +1,54 @@
+package lab.pkg3;
+
+
+public class Lab3 {
+
+ int suma(int[] liczby){
+    return liczby[0]+liczby[1];
+ }
+    public static void main(String[] args) {
+           Lab3 zad = new Lab3();
+   int[] liczby = {1, 2};
+   System.out.println(zad.suma(liczby));
+    }
+    /*    String name = "Jan - ' cytat' ";
+        int dlugosc = name.length();
+    System.out.println("dlugosc sringa : "+dlugosc);
+   String Dname = name.toUpperCase();
+      String Mname = name.toLowerCase();
+      int index = name.indexOf("an");
+      name.lastIndexOf("an");
+      name.replace('a','A');
+   System.out.println("Duze litery : "+Dname+ "Male litery : "+Mname);
+   String nazwisko = "nowak";
+   String IM = name + " " + nazwisko;
+    */
+    
+    
+ /*   String[] tablica={"a", "b", "c"};
+    int [] tabInt={2,3,4,524,-23};
+    String items[][]= {{"a","b","c"},{"d","e","f"}};
+    
+    System.out.println((tabInt[2]));
+        System.out.println(tabInt.length);
+        for (int i=0; i<tablica.length; i++)
+            System.out.println(tablica[i]);
+        
+        */
+
+
+
+    
+
+    
+    
+}
+
+        
+        
+    
+    
+    
+    
+   
+    
